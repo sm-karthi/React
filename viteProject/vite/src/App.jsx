5c9f27fb2d9d8a4920b1870dd2dac6b50efcd6c6
sm-karthi <karthikeyan24022007@gmail.com>
2025-06-02 17:55:50 +0530

    Installed the React Vite

---
+++ viteProject/vite/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-blue-600 h-64 w-32 flex items-center justify-center bg-gray-100 rounded-lg shadow-lg'>Hello</div>
+    </>
+  )
+}
+
+export default App
